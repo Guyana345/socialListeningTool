ec8427def57ea8a89b72989f5556567819c9017f
Harjass S. Kukreja <attahphol.kukreja@students.stamford.edu>
2018-10-02 12:05:11 +0700

    Add files via upload

---
+++ social-media/01-apis/scripts/secrets.py
@@ -0,0 +1,12 @@
+# Twitter API credentials, you can get them here https://apps.twitter.com/
+TWITTER_C_KEY = ""
+TWITTER_C_SECRET = ""
+TWITTER_A_KEY = ""
+TWITTER_A_SECRET = ""
+
+# Facebook API credentials
+FACEBOOK_APP_ID = "2377949572221031"
+FACEBOOK_APP_SECRET = "9774ca88e1ecba659ea6d70279a251e6"
+
+# YouTube credentials
+YOUTUBE_API_KEY = "AIzaSyDq_50Ftzpo6q3BDiY1XOKsWTFl0QvTFag"
